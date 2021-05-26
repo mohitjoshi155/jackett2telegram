36ab38cac9ce7e73c6d18452a547e113e3a0869f
mohitjoshi155 <58655292+mohitjoshi155@users.noreply.github.com>
2021-05-26 10:58:13 +0530

    Update jackett2telegram.py

--- jackett2telegram.py
+++ jackett2telegram.py
@@ -368,12 +368,6 @@ def jackettitem_to_telegram(context: CallbackContext, item: ElementTree.Element,
                uploadvolumefactor)
 
     link = item.find('link').text
-            
-    if not link:
-    msg = link + '\n'
-    # Sending message
-    bot.send_message(client.chat_id, msg)
-    
     if (link.startswith("magnet:")):
         keyboard = [
             [
